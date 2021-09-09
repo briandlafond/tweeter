41109d44f668a66b01260f19a0f0c65f26975d69
Brian LaFond <brian.d.lafond@gmail.com>
2021-09-09 21:19:39 +0000

    add char counter functionality and style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-  console.log('hello');
-});
+
+    $(".new-tweet").on('input', function() {    
+      let input = $(this);    
+      let form = input.closest('form');    
+      let counter = form.find('.counter')
+      let newTweet = $(this).val().length;
+      let charactersLeft = 140 - newTweet;    
+      counter.html(charactersLeft);
+      if (newTweet > 140) {
+        counter.addClass('tweetTooLong');
+      } else if (newTweet <= 140) {
+        counter.removeClass('tweetTooLong');
+      }
+   });
+ 
+ });
+
+
+
