f0d218261b922111a670ee38b48c12ecd448a1d2
Brian LaFond <brian.d.lafond@gmail.com>
2021-09-10 20:43:04 +0000

    add tweet container style and time ago functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
 
-    $(".new-tweet").on('input', function() {    
-      let input = $(this);    
-      let form = input.closest('form');    
-      let counter = form.find('.counter')
-      let newTweet = $(this).val().length;
-      let charactersLeft = 140 - newTweet;    
-      counter.html(charactersLeft);
-      if (newTweet > 140) {
-        counter.addClass('tweetTooLong');
-      } else if (newTweet <= 140) {
-        counter.removeClass('tweetTooLong');
-      }
-   });
- 
+  $(".new-tweet").on('input', function() {    
+    let input = $(this);    
+    let form = input.closest('form');    
+    let counter = form.find('.counter')
+    let newTweet = $(this).val().length;
+    let charactersLeft = 140 - newTweet;    
+    counter.html(charactersLeft);
+    if (newTweet > 140) {
+      counter.addClass('tweetTooLong');
+    } else if (newTweet <= 140) {
+      counter.removeClass('tweetTooLong');
+    }
+  });
+
+
  });
 
 
